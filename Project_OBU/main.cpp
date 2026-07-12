d98e17449d1aa1fb948893bae79f0deb481da107
jina555 <kimjina0309@naver.com>
2026-07-12 17:50:28 +0900

    윈도우 창 띄우기

---
+++ Project_OBU/main.cpp
@@ -0,0 +1,41 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+	WNDCLASS wc = {};
+
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = TEXT("MyWindow");
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindow(TEXT("MyWindow"), TEXT("첫 번째 윈도우"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		800, 600, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	MSG msg;
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
