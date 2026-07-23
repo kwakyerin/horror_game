2e0a075366e190de2e2ff2c66ec5ffd42af3fe32
jina555 <kimjina0309@naver.com>
2026-07-24 01:59:01 +0900

    까막귀신 클래스 작성 중, 헤더파일 작성 중

---
+++ kkamakGhost.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <windows.h>
+#include<gdiplus.h>
+
+#include "Character.h"
+
+class KkamakGhost {
+private:
+	float x;
+	float y;
+	float moveSpeed;
+
+	Gdiplus::Image* image;
+
+public:
+
+};
